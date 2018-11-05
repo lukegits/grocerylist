39176852e386879b5c64df0b77fa848e8af0e841
Lukegits <luket.chavez@gmail.com>
2018-11-05 16:20:25 -0700

    sign in and out now working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :lists
   root 'lists#index'
   resources :users, only: [:new, :create]
-  
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
